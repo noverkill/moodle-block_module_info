8889004dc402803ce558d0cc77be0dcc872d8c2b
ian.wild <ian.wild@heavy-horse.co.uk>
2013-07-27 22:08:36 +0100

    Added functionality to render documents.

--- renderer.php
+++ renderer.php
@@ -161,6 +161,61 @@ class block_module_info_renderer extends plugin_renderer_base {
         return $result;
         
     }
+    
+    /**
+     * Internal function - creates htmls structure suitable for YUI tree.
+     */
+    protected function htmllize_document_tree($context, $block_context, $dir) {
+        global $CFG;
+        
+        $result = '';
+        
+        $yuiconfig = array();
+        $yuiconfig['type'] = 'html';
+    
+        if (empty($dir['subdirs']) and empty($dir['files'])) {
+            return '';
+        }
+        $result = '<ul>';
+        foreach ($dir['subdirs'] as $subdir) {
+            $image = $this->output->pix_icon("f/folder", $subdir['dirname'], 'moodle', array('class'=>'icon'));
+            $result .= '<li yuiConfig=\''.json_encode($yuiconfig).'\'><div>'.$image.s($subdir['dirname']).'</div> '.$this->htmllize_document_tree($context, $block_context, $subdir).'</li>';
+        }
+        foreach ($dir['files'] as $file) {
+            $url = file_encode_url("$CFG->wwwroot/pluginfile.php", '/'.$context->id.'/block_module_info/documents/'.$block_context->id.$file->get_filepath().$file->get_filename(), true);
+            $filename = $file->get_filename();
+            $icon = mimeinfo("icon", $filename);
+            $image = $this->output->pix_icon("f/$icon", $filename, 'moodle', array('class'=>'icon'));
+            $result .= '<li yuiConfig=\''.json_encode($yuiconfig).'\'><div>'.html_writer::link($url, $image.$filename).'</div></li>';
+        }
+        $result .= '</ul>';
+    
+        return $result;
+    }
+    
+    public function get_document_tree($block_context) {
+        global $USER;
+        
+        $result = html_writer::tag('h2', get_string( 'documents_header', 'block_module_info' ),
+                array('class'=>'documents'));
+        
+        // Get the stored files
+        $fs = get_file_storage();
+        $dir = $fs->get_area_tree($this->page->context->id, 'block_module_info', 'documents', $block_context->id);
+        
+        $module = array('name'=>'block_module_info', 'fullpath'=>'/blocks/module_info/module.js', 'requires'=>array('yui2-treeview'));
+        if (empty($dir['subdirs']) && empty($dir['files'])) {
+            $result = $this->output->box(get_string('nofilesavailable', 'repository'));
+        } else {
+            $htmlid = 'document_tree_'.uniqid();
+            $this->page->requires->js_init_call('M.block_module_info.init_tree', array(false, $htmlid));
+            $result .= '<div id="'.$htmlid.'">';
+            $result .= $this->htmllize_document_tree($this->page->context, $block_context, $dir);
+            $result .= '</div>';
+        }
+        
+        return $result;
+    }
 
     /**
      * Return true for now
