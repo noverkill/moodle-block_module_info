4b8957063158cd351eb62f3aeb1b83c49db1d2bc
unknown <wildi@BNNV54J-LCR.lrh-tr.nhs.uk>
2013-08-01 17:04:24 +0100

    Implemented expanding and collapsing of sections.

--- module.js
+++ module.js
@@ -21,21 +21,156 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+var additional_teachers_heading = null;
+var documents_heading = null;
+var schedule_heading = null;
+
+function hide_additional_teachers(e) {
+	if(additional_teachers_heading) {
+		e.halt();
+		// Change heading class
+		var span = additional_teachers_heading.one('span');
+		if(span) {
+			span.removeClass('expanded');
+			span.addClass('collapsed');
+		}
+		// Hide all additional teachers
+		var pane = additional_teachers_heading.get('nextSibling');
+		if(pane) {
+			pane.setAttribute("hidden", true);
+		}
+		// Attach new event handler
+		additional_teachers_heading.detach('click', this.hide_additional_teachers, false);
+		additional_teachers_heading.on('click', this.show_additional_teachers, this);
+	}
+}
+		
+function show_additional_teachers(e) {
+	if(additional_teachers_heading) {
+		e.halt();
+		// Change heading class
+		var span = additional_teachers_heading.one('span');
+		if(span) {
+			span.removeClass('collapsed');
+			span.addClass('expanded');
+		}
+		// Hide all additional teachers
+		var pane = additional_teachers_heading.get('nextSibling');
+		if(pane) {
+			pane.removeAttribute("hidden");
+		}
+		// Attach new event handler
+		additional_teachers_heading.detach('click', this.show_additional_teachers, false);
+		additional_teachers_heading.on('click', this.hide_additional_teachers, this);
+	}
+}
+
+function hide_documents(e) {
+	if(documents_heading) {
+		e.halt();
+		// Change heading class
+		var span = documents_heading.one('span');
+		if(span) {
+			span.removeClass('expanded');
+			span.addClass('collapsed');
+		}
+		// Hide all documents
+		var pane = documents_heading.get('nextSibling');
+		if(pane) {
+			pane.setAttribute("hidden", true);
+		}
+		// Attach new event handler
+		documents_heading.detach('click', this.hide_documents, false);
+		documents_heading.on('click', this.show_documents, this);
+	}
+}
+		
+function show_documents(e) {
+	if(documents_heading) {
+		e.halt();
+		// Change heading class
+		var span = documents_heading.one('span');
+		if(span) {
+			span.removeClass('collapsed');
+			span.addClass('expanded');
+		}
+		// Hide all documents
+		var pane = documents_heading.get('nextSibling');
+		if(pane) {
+			pane.removeAttribute("hidden");
+		}
+		// Attach new event handler
+		documents_heading.detach('click', this.show_documents, false);
+		documents_heading.on('click', this.hide_documents, this);
+	}
+}
+		
+function hide_schedule(e) {
+	if(schedule_heading) {
+		e.halt();
+		// Change heading class
+		var span = schedule_heading.one('span');
+		if(span) {
+			span.removeClass('expanded');
+			span.addClass('collapsed');
+		}
+		// Hide all sessions
+		var pane = schedule_heading.get('nextSibling');
+		if(pane) {
+			pane.setAttribute("hidden", true);
+		}
+		// Attach new event handler
+		schedule_heading.detach('click', this.hide_schedule, false);
+		schedule_heading.on('click', this.show_schedule, this);
+	}
+}
+		
+function show_schedule(e) {
+	if(schedule_heading) {
+		e.halt();
+		// Change heading class
+		var span = schedule_heading.one('span');
+		if(span) {
+			span.removeClass('collapsed');
+			span.addClass('expanded');
+		}
+		// Show all sessions
+		var pane = schedule_heading.get('nextSibling');
+		if(pane) {
+			pane.removeAttribute("hidden");
+		}
+		// Attach new event handler
+		schedule_heading.detach('click', this.show_schedule, false);
+		schedule_heading.on('click', this.hide_schedule, this);
+	}
+}
+
 M.block_module_info = {};
 
-M.block_module_info.init_tree = function(Y, expand_all, htmlid) {
-    Y.use('yui2-treeview', function(Y) {
-        var tree = new YAHOO.widget.TreeView(htmlid);
+M.block_module_info.init_document_tree = function(Y, expand_all, htmlid) {
+		Y.use('yui2-treeview', function(Y) {
+	
+			additional_teachers_heading = Y.one('#additional-teachers-heading'); 
+			additional_teachers_heading.on('click', hide_additional_teachers, this);
+			
+			documents_heading = Y.one('#documents-heading'); 
+			documents_heading.on('click', hide_documents, this);
+			
+			schedule_heading = Y.one('#schedule-heading'); 
+			schedule_heading.on('click', hide_schedule, this);
+			
+			var tree = new YAHOO.widget.TreeView(htmlid);
 
-        tree.subscribe("clickEvent", function(node, event) {
-            // we want normal clicking which redirects to url
-            return false;
-        });
+			tree.subscribe("clickEvent", function(node, event) {
+				// we want normal clicking which redirects to url
+				return false;
+			});
 
-        if (expand_all) {
+			if (expand_all) {
             tree.expandAll();
-        }
+			}
 
-        tree.render();
-    });
+			tree.render();
+		});
+		
 };
