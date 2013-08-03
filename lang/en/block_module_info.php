46fa3d9e78247a049afc8b7a737ee74d9e1e41b7
ian.wild <ian.wild@heavy-horse.co.uk>
2013-08-03 21:10:02 +0100

    Added new language strings and tidied code.

--- lang/en/block_module_info.php
+++ lang/en/block_module_info.php
@@ -3,34 +3,34 @@
     $string['module_info'] = 'Module Info';
 	
 	//Config Form DB Connection Section
-	$string['noconnection']						=	'No Connection';
-    $string[ 'mis_connection' ]                 =   'MIS Connection';
-    $string[ 'mis_connection_desc' ] = 'Specify connection to the database containing course module data.';
+	$string['noconnection'] = 'No Connection';
+    $string['mis_connection'] = 'MIS Connection';
+    $string['mis_connection_desc'] = 'Specify connection to the database containing course module data.';
     
-    $string[ 'db_connection' ]                  =   'DB Connection';
-    $string[ 'db_name' ]                        =   'DB Name';
-    $string[ 'set_db_name' ]                    =   'set the name of the DB';
-    $string[ 'db_prefix' ]                      =   'DB Prefix';
-    $string[ 'prefix_for_tablenames' ]          =   'Prefix for tablenames (if any)';
-    $string[ 'db_host' ]                        =   'DB host';
-    $string[ 'host_name_or_ip' ]                =   'DB Hostname or IP';
-    $string[ 'db_table' ]                       =   'DB table';
-    $string[ 'db_pass' ]                        =   'DB Password';
-    $string[ 'db_user' ]                        =   'DB Username';
+    $string['db_connection'] = 'DB Connection';
+    $string['db_name'] = 'DB Name';
+    $string['set_db_name'] = 'set the name of the DB';
+    $string['db_prefix'] = 'DB Prefix';
+    $string['prefix_for_tablenames'] = 'Prefix for tablenames (if any)';
+    $string['db_host'] = 'DB host';
+    $string['host_name_or_ip'] = 'DB Hostname or IP';
+    $string['db_table'] = 'DB table';
+    $string['db_pass'] = 'DB Password';
+    $string['db_user'] = 'DB Username';
 	
 	//Config Data Mapping Fields
-	$string[ 'data_mapping' ]                   =   'Data Mapping';
-	$string[ 'data_mapping_desc' ]            = 'The name of the field in the remote table that maps onto the given variable.';
-    $string[ 'extcourseid' ]                    =   'Course ID';
-    $string[ 'extcourseiddesc' ]                =   'This field should map to course->idnumber in Moodle';
-	$string[ 'module_code' ]                    =   'Code';
-	$string[ 'module_level' ]                   =   'Level';
-	$string[ 'module_credit' ]                  =   'Credit Value';
-	$string[ 'module_semester']					=	'Semester';
-	$string[ 'convenor' ]                    	=   'Convenor';
-	$string[ 'convenorid' ]                    	=   'Convenor Identifier';
-	$string[ 'convenor_name' ]                  =   'Convenor Name';
-	$string[ 'noid' ] 							= 	'No ID';
+	$string['data_mapping'] = 'Data Mapping';
+	$string['data_mapping_desc'] = 'The name of the field in the remote table that maps onto the given variable.';
+    $string['extcourseid']  = 'Course ID';
+    $string['extcourseiddesc'] = 'This field should map to course->idnumber in Moodle';
+	$string['module_code'] = 'Code';
+	$string['module_level'] = 'Level';
+	$string['module_credit'] = 'Credit Value';
+	$string['module_semester'] = 'Semester';
+	$string['convenor'] = 'Convenor';
+	$string['convenorid'] = 'Convenor Identifier';
+	$string['convenor_name']  = 'Convenor Name';
+	$string['noid'] = 'No ID';
 	
 	// SMART Timetabling
 	$string['setting_header_smart'] = "SMART Timtable Settings";
@@ -57,93 +57,101 @@
 	$string['setting_style'] = 'Timetabling Style';
 	$string['setting_style_desc'] = 'Timetabling Style';
 	
+	$string['default_personal_smart_link'] = 'Personal timetable';
+	$string['student_personal_smart_link'] = 'Personal timetable';
+	$string['staff_personal_smart_link'] = 'Personal timetable';
+	
+	$string['default_module_smart_link'] = 'Module timetable';
+	$string['student_module_smart_link'] = 'Module timetable';
+	$string['staff_module_smart_link'] = 'Module timetable';
+	
 	//Config Defaults
-	$string[ 'default' ]                        =   'Defaults';
-	$string[ 'default_desc' ]                        =   'Block default configuration settings can be specified here.';
+	$string['default'] = 'Defaults';
+	$string['default_desc'] = 'Block default configuration settings can be specified here.';
 	
-	$string[ 'convenor_role_name_options' ]    =    'Module owner names';
-	$string[ 'convenor_role_name_options_desc']=    'Possible valid alternative names for the module owner are \'Convenor\' or \'Module Organiser\', for instance. Write each option on a new line.';
-	$string[ 'additional_teacher_role_name_options' ]    =    'Additional teacher names';
-	$string[ 'additional_teacher_role_name_options_desc']=    'Possible valid alternative names for additional teachers are \'Additional teachers\' or \'Teaching assistants\', for example. Write each option on a new line.';
+	$string['convenor_role_name_options'] = 'Module owner names';
+	$string['convenor_role_name_options_desc'] = 'Possible valid alternative names for the module owner are \'Convenor\' or \'Module Organiser\', for instance. Write each option on a new line.';
+	$string['additional_teacher_role_name_options'] = 'Additional teacher names';
+	$string['additional_teacher_role_name_options_desc'] = 'Possible valid alternative names for additional teachers are \'Additional teachers\' or \'Teaching assistants\', for example. Write each option on a new line.';
 	
-	$string[ 'defaulthtml' ]                    =   'Default HTML';
+	$string['defaulthtml'] = 'Default HTML';
 
     //Customisations
     
 	// Core info
-	$string[ 'core_info_header']                =   'Core info';
-	$string[ 'override_setting' ]              =    'Enter custom setting here';
-    $string[ 'config_title' ]                   =   'Title';
-    $string[ 'config_module_code' ]             =   'Display Module Code';
-    $string[ 'config_module_code_override']     =   'Custom Code';
-    $string[ 'config_module_level' ]            =   'Display Module Level';
-    $string[ 'config_module_level_override']    =   'Override Level';
-    $string[ 'config_module_credit' ]           =   'Display Module Credits';
-    $string[ 'config_module_credit_override' ]  =   'Override Credit Value';
-    $string[ 'config_module_semester' ]         =   'Display Module Semester';
-    $string[ 'config_module_semester_override' ] =   'Override Semester';
-    
+	$string['core_info_header'] = 'Core info';
+	$string['override_setting'] = 'Enter custom setting here';
+    $string['config_title'] = 'Title';
+    $string['config_module_code'] = 'Display Module Code';
+    $string['config_module_code_override'] = 'Custom Code';
+    $string['config_module_level'] = 'Display Module Level';
+    $string['config_module_level_override'] = 'Override Level';
+    $string['config_module_credit'] = 'Display Module Credits';
+    $string['config_module_credit_override'] = 'Override Credit Value';
+    $string['config_module_semester'] = 'Display Module Semester';
+    $string['config_module_semester_override'] = 'Override Semester';
+
     // Person display options
-    $string[ 'additional_person_display_options' ] = 'Global profile display options';
-    $string[ 'additional_person_display_options_desc' ] = 'Course owners can choose to display the profile fields selected here.';
+    $string['additional_person_display_options'] = 'Global profile display options';
+    $string['additional_person_display_options_desc'] = 'Course owners can choose to display the profile fields selected here.';
     
     // Teaching
-    $string[ 'teaching_header']                 =   'Teaching';
+    $string['teaching_header'] = 'Teaching';
     
-    $string[ 'teacher_headings_options_not_configured' ] = 'Headings options not configured';
+    $string['teacher_headings_options_not_configured'] = 'Headings options not configured';
     
-    $string[ 'location' ] = 'Location';
-    $string[ 'officehours' ] = 'Office hours';
-    $string[ 'profilepic' ] = 'Profile picture';
-    $string[ 'profilepic_size' ] = 'Profile picture size';
-    $string[ 'small' ] = 'Small';
-    $string[ 'large' ] = 'Large';
-    $string[ 'config_module_owner_heading' ]    =   'Module Owner heading';
-    $string[ 'config_display_convenor_options' ] =	'Module owner display options';
-    $string[ 'config_convenor_name_override' ]  =   'Module Owner name override';
-    $string[ 'config_convenor_email_override' ] = 'Module owner email override';
-    $string[ 'config_convenor_location_override' ]  =   'Module owner location override';
-    $string[ 'config_display_convenor_office_hours']  =    'Display module owner office hours';
-    $string[ 'config_convenor_office_hours_override' ] = 'Module owner office hours override';
-    $string[ 'config_display_convenor_personal_webpage' ] = 'Display personal webpage';
+    $string['location'] = 'Location';
+    $string['officehours'] = 'Office hours';
+    $string['profilepic'] = 'Profile picture';
+    $string['profilepic_size'] = 'Profile picture size';
+    $string['small'] = 'Small';
+    $string['large'] = 'Large';
+    $string['config_module_owner_heading'] = 'Module Owner heading';
+    $string['config_display_convenor_options'] = 'Module owner display options';
+    $string['config_convenor_name_override'] = 'Module Owner name override';
+    $string['config_convenor_email_override'] = 'Module owner email override';
+    $string['config_convenor_location_override'] = 'Module owner location override';
+    $string['config_display_convenor_office_hours'] = 'Display module owner office hours';
+    $string['config_convenor_office_hours_override'] = 'Module owner office hours override';
+    $string['config_display_convenor_personal_webpage'] = 'Display personal webpage';
     
     // Additional teachers
-    $string[ 'additional_teachers_header' ]     = 'Additional teachers';
-    $string[ 'hide_additional_teachers_heading'] = 'None';
-    $string[ 'noadditionalteachersavailable' ]            = 'No additonal teachers configured';
+    $string['additional_teachers_header'] = 'Additional teachers';
+    $string['hide_additional_teachers_heading'] = 'None';
+    $string['noadditionalteachersavailable'] = 'No additonal teachers configured';
+
+    $string['config_additional_teachers_heading'] = 'Additional teachers heading';
+    $string['config_additional_teachers_heading_default'] = 'e.g. GTAs, TAs';
+    $string['config_display_additional_teacher_options'] = 'Additional teacher display options';
     
-    $string[ 'config_additional_teachers_heading' ] = 'Additional teachers heading';
-    $string[ 'config_additional_teachers_heading_default' ] = 'e.g. GTAs, TAs';
-    $string[ 'config_display_additional_teacher_options' ] =	'Additional teacher display options';
-    
-    $string[ 'config_additional_teacher' ]     =    'Additional teacher';
-    $string[ 'config_additional_teacher_name' ] =     'Name';
-    $string[ 'config_additional_teacher_email' ] =     'Email';
-    $string[ 'config_additional_teacher_location' ] =     'Location';
-    $string[ 'config_additional_teacher_office_hours' ] =     'Office hours';
+    $string['config_additional_teacher'] = 'Additional teacher';
+    $string['config_additional_teacher_name'] = 'Name';
+    $string['config_additional_teacher_email'] = 'Email';
+    $string['config_additional_teacher_location'] = 'Location';
+    $string['config_additional_teacher_office_hours'] = 'Office hours';
     
     // Schedule
-    $string[ 'schedule_header' ]                = 'Schedule';
-    $string[ 'config_enable_personal_timetable_link' ] = 'Display personal timetable link (SMART)';
-    $string[ 'config_enable_module_timetable_link' ] = 'Display module timetable link (SMART)';
+    $string['schedule_header'] = 'Schedule';
+    $string['config_enable_personal_timetable_link'] = 'Display personal timetable link (SMART)';
+    $string['config_enable_module_timetable_link'] = 'Display module timetable link (SMART)';
     
-    $string[ 'config_additional_session' ]     =    'Additional teaching session';
-    $string[ 'config_additional_session_subheading' ] =     'Teaching subheading';
-    $string[ 'config_additional_session_day' ] =     'Day';
-    $string[ 'config_additional_session_time' ] =     'Time';
-    $string[ 'config_additional_session_location' ] =     'Location';
+    $string['config_additional_session'] = 'Additional teaching session';
+    $string['config_additional_session_subheading'] = 'Teaching subheading';
+    $string['config_additional_session_day'] = 'Day';
+    $string['config_additional_session_time'] = 'Time';
+    $string['config_additional_session_location'] = 'Location';
     
-    $string[ 'session_details' ] = '{$a->day} {$a->time} in {$a->location}';
-    $string[ 'nosessionsavailable' ]            = 'No extra sessions configured';
+    $string['session_details'] = '{$a->day} {$a->time} in {$a->location}';
+    $string['nosessionsavailable'] = 'No extra sessions configured';
     
     // Documents
-    $string[ 'documents_header' ]                = 'Documents';
+    $string['documents_header'] = 'Documents';
    
     // Legacy
-    $string[ 'legacy_header']                 =   'Legacy';
+    $string['legacy_header'] = 'Legacy';
     
-    $string[ 'convenor_not_found']              =   ' not found in QMplus database';
+    $string['convenor_not_found'] = ' not found in QMplus database';
     
-    $string[ 'config_html' ]                    =   'Display HTML';
-    $string[ 'config_htmlcontent' ]             =   'HTML Content';
-    $string[ 'reset' ]                          =   'Reset HTML - cannot be undone';
+    $string['config_html'] = 'Display HTML';
+    $string['config_htmlcontent'] = 'HTML Content';
+    $string['reset'] = 'Reset HTML - cannot be undone';
