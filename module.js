f1bb3ba9c31a2479cb2bb1d0f486c5a8c2c7651a
noverkill <szilard@szilard.co.uk>
2014-07-31 10:54:52 +0100

    Fixed YUI problem

--- module.js
+++ module.js
@@ -27,7 +27,7 @@ M.block_module_info.init = function(Y, expand_all, htmlid) {
 			
 	Y.use('yui2-treeview', function(Y) {
 
-		var tree = new YAHOO.widget.TreeView(htmlid);
+		var tree = new Y.YUI2.widget.TreeView(htmlid);
 
 		tree.subscribe("clickEvent", function(node, event) {
 			// we want normal clicking which redirects to url
